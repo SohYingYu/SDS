776ebaa921dbd226ed6845a00a3ec9624489858d
Yang1900000 <yangzy1900@gmail.com>
2024-12-12 00:25:17 +0800

    is thu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,25 @@ import Bottombar from './components/Bottombar/Bottombar';
 
 const App = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isBottombarOpen, setIsBottombarOpen] = useState(true);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const toggleBottombar = () => {
+    setIsBottombarOpen(!isBottombarOpen);
   };
 
   return (
     <div className="app">
       <Mapbox />
       <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <Bottombar isSidebarOpen={isSidebarOpen} />
+      <Bottombar 
+        isSidebarOpen={isSidebarOpen} 
+        isBottombarOpen={isBottombarOpen} 
+        toggleBottombar={toggleBottombar} 
+      />
     </div>
   );
 };
